af637ae5f4bcb4c8a7c690eb4335875c15113594
mWasyluk <marekwasyluk@wp.pl>
2023-12-20 18:46:55 +0100

    feat: upload Snake.exe ref: provide fat jar dependency ref: add intermediary launch class

---
+++ src/main/java/pl/mwasyluk/games/snake/Launcher.java
@@ -0,0 +1,9 @@
+package pl.mwasyluk.games.snake;
+
+// Required by JavaFX to execute class that extends Application
+public class Launcher {
+
+    public static void main(String[] arguments){
+        SnakeApplication.main(arguments);
+    }
+}
